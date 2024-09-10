56ea99a2be112679d627383abc22087974d1f571
Antonella <168837149+AntonellaEL@users.noreply.github.com>
2024-09-10 11:17:24 +0200

    init

---
+++ src/main/java/dev/anto/ProyectoPersonalApplication.java
@@ -0,0 +1,13 @@
+package dev.anto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPersonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/anto/ProyectoPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
