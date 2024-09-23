511a9034af4df73bd14f36f5575bb88720c7047c
Antonella <168837149+AntonellaEL@users.noreply.github.com>
2024-09-23 17:50:01 +0200

    created delete

--- src/main/java/dev/anto/controllers/ProductoController.java
+++ src/main/java/dev/anto/controllers/ProductoController.java
@@ -5,6 +5,7 @@ import dev.anto.services.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,6 +66,13 @@ public class ProductoController {
     public ResponseEntity<Producto> createProducto(@RequestBody Producto producto) {
         Producto createdProducto = productoService.createProducto(producto);
         return ResponseEntity.status(201).body(createdProducto);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> deleteProducto(@PathVariable Long id) {
+        productoService.deleteProducto(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
