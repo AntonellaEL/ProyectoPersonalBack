cb6f5f9f65d7f4f49959a432398b9fd57e520fab
Antonella <168837149+AntonellaEL@users.noreply.github.com>
2024-09-23 17:50:16 +0200

    delete in service

--- src/main/java/dev/anto/services/ProductoService.java
+++ src/main/java/dev/anto/services/ProductoService.java
@@ -41,4 +41,8 @@ public class ProductoService {
     public Producto createProducto(Producto producto) {
         return productoRepository.save(producto);
     }
+
+    public void deleteProducto(Long id) {
+        productoRepository.deleteById(id);
+    }
 }
